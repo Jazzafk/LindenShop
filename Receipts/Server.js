eb4715dc4a1b7c9c121838e7d064a98523ff47b3
kk-linden <77739454+kk-linden@users.noreply.github.com>
2022-02-14 14:27:24 -0500

    Database in progress

---
+++ Receipts/Server.js
@@ -0,0 +1,41 @@
+const http = require("http"); //looks for a package with i
+const fs = require('fs'); // for reading and creating files
+const PORT = 3000; //port 3000 is not usually used
+http.createServer((req, res) => { // function that creates a local server
+    const type = req.method; //
+    const url = req.url; //this checks the url after the / and asks server for that
+    console.log(type, url);
+    if (type == "GET") {
+        if (url == "/receipts.html") {
+            fs.readFile("./receipt.html", (err, data) => {
+                res.end(data);
+            })
+            fs.readFile("./info.txt", "utf8", (err, data2) => {
+                res.write(data2)
+                res.end();
+            })
+        }
+    }
+
+    if (type == "POST") {
+    let body = ""
+    console.log("post request");
+    req.on("data", chunk => {
+        console.log("chunks", chunk)
+        body += chunk
+    });
+    req.on("end", () => {
+        console.log("body", body)
+        res.end("success")
+        fs.writeFile('./info.txt', body, err => {
+            if (err) {
+                console.error(err)
+                return
+            }
+            console.log("working!")
+        })
+    })
+}
+}).listen(PORT);
+
+//http://localhost:3000/receipt.html
--- Receipts/receipts.js
+++ Receipts/receipts.js
@@ -3,3 +3,8 @@
 // create database
 //add html file with receipts to the file
 // get sends the information to the user's computer
+// have functions for people to push things to the database
+// have a text file that stores the information
+
+
+//in the fetch request, add fetch requests for both files in a function so the user can do it whenever they want
