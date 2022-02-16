2dc69a63920c9b858583f8a2d295e35230c3a7f3
kk-linden <77739454+kk-linden@users.noreply.github.com>
2022-02-16 13:37:54 -0500

    sending receipts

--- Functions/receipts.js
+++ Functions/receipts.js
@@ -4,8 +4,13 @@ function getReceipts() {
     }).then(function (result) {
         var obj = result;
         console.log(obj);
-        makeText(obj, ELEMENT)
+        makeText(obj, para) // para can be replaced.
     })
 } 
-
 // this function would allow you to add the contents of the text file into the browser.
+
+function SendReceipts(){
+
+}
+
+//this function would allow you to add the receipts to the text file (to be added to the browser with the function above)
