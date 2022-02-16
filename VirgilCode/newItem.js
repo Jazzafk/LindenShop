f3d8b485d5d25a80913a7248983bf90158a892be
unknown <alisong@lindenschool.ca>
2022-02-16 12:51:46 -0500

    making files

---
+++ VirgilCode/newItem.js
@@ -0,0 +1,22 @@
+var products = {
+    book: {
+        price: 30,
+        barcodeNum: '840374958374',
+        img: '',
+        numberStock: 69
+    },
+    rockets: {
+        price: 200,
+        barcodeNum: '849274655483',
+        img: '',
+        numberStock: 420
+    }
+}
+
+function displaySingle(item){
+    
+}
+
+function displayAll(){
+
+}
--- newItem.js
+++
@@ -1,8 +0,0 @@
-var products = {
-    book: {
-        price: 30,
-        barcodeNum: '840374958374',
-        img: '',
-        numberStock: 69
-    }
-}
