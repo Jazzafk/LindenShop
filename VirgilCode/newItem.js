1aae2b9b1e64608bcb34ee0c9f10ab59b7a346af
unknown <alisong@lindenschool.ca>
2022-02-22 10:57:00 -0500

    finishing functions and starting css

--- VirgilCode/newItem.js
+++ VirgilCode/newItem.js
@@ -1,22 +1,46 @@
 var products = {
     book: {
-        price: 30,
+        name: 'book',
+        price: 30.00,
         barcodeNum: '840374958374',
-        img: '',
+        img: 'https://images-na.ssl-images-amazon.com/images/I/91i7MSrNHXL.jpg',
         numberStock: 69
     },
     rockets: {
-        price: 200,
+        name:'rockets',
+        price: 200.00,
         barcodeNum: '849274655483',
-        img: '',
+        img: 'https://i5.walmartimages.ca/images/Enlarge/165/126/6000203165126.jpg',
         numberStock: 420
     }
 }
 
-function displaySingle(item){
-    
+// var allProd = document.createElement('div');
+// document.body.appendChild(allProd);
+
+function displaySingle(item, allProd){
+    var prodDiv = document.createElement('div');
+    allProd.appendChild(prodDiv);
+    prodDiv.classList.add('prodDivClass');
+    var prodImg = document.createElement('img');
+    prodDiv.appendChild(prodImg);
+    prodImg.setAttribute('src', products[item]['img']);
+    prodImg.classList.add('prodImgClass');
+    var prodName = document.createElement('h3');
+    prodDiv.appendChild(prodName);
+    makeText(products[item]['name'], prodName);
+    var prodPrice = document.createElement('h4');
+    prodDiv.appendChild(prodPrice);
+    makeText('$' + (products[item]['price']), prodPrice);
 }
 
-function displayAll(){
-
+function displayAll(container){
+    var allProd = document.createElement('div');
+    container.appendChild(allProd);
+    var allProdArr = Object.keys(products);
+    for (i=0;i<allProdArr.length;i++){
+        displaySingle(allProdArr[i], allProd);
+    }
 }
+
+displayAll(document.getElementById('productContainer'));
